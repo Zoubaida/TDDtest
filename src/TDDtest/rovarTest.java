142b2a4e3af9a1a22362100e6445386445fa3b81
Zoubaida <zozo_tofa7e@live.se>
2018-09-28 18:19:32 +0200

    Testcases

--- src/TDDtest/rovarTest.java
+++ src/TDDtest/rovarTest.java
@@ -1,19 +1,40 @@
 package TDDtest;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
 class rovarTest {
-
-	@Test
+rovar r = new rovar();
+	@Test	
 	void testEnrov() {
-		fail("Not yet implemented");
+		
+		assertEquals("nonorormomalol",  rovar.enrov("normal"));
+	
+		
+		assertEquals("zozoo", rovar.enrov("zoo"));
+		
+		
+		assertEquals("dodooror", rovar.enrov("door"));
+		
+		assertEquals("WOWAROR", rovar.enrov("WAR"));
+		
+		String normal = null;
+		assertEquals(null, rovar.enrov(normal));
 	}
 
 	@Test
 	void testDerov() {
-		fail("Not yet implemented");
+		
+		assertEquals("normal", rovar.derov("nonorormomalol"));
+		
+		assertEquals("no", rovar.derov("nono"));
+		
+		assertEquals("zero", rovar.derov("zozeroro"));
+		
+		String rov = null;
+		assertEquals( null , rovar.derov(rov));
 	}
 
-}
+} 
