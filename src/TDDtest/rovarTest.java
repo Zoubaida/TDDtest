66d3fa80e174daf5d6ebc75e9b0ba07e92c4dc35
Zoubaida <zozo_tofa7e@live.se>
2018-09-28 03:47:09 +0200

    start of TDD lab
    skapade en vanlig java class och en tillhörande Junit class

---
+++ src/TDDtest/rovarTest.java
@@ -0,0 +1,19 @@
+package TDDtest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class rovarTest {
+
+	@Test
+	void testEnrov() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDerov() {
+		fail("Not yet implemented");
+	}
+
+}
